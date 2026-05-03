c37c9204716a4d0a6ca004ebbeb46bae710ba622
Kushal <kushalsingha00@gmail.com>
2026-05-03 09:12:31 +0530

    chore: fixed the code indentation

--- static/js/main.js
+++ static/js/main.js
@@ -4,21 +4,17 @@ const fileInfo = document.getElementById('fileInfo')
 const form = document.getElementById('form')
 const loader = document.getElementById('loader')
 
-// Open file picker
 dropZone.addEventListener('click', () => input.click())
 
-// Drag over
 dropZone.addEventListener('dragover', e => {
   e.preventDefault()
   dropZone.classList.add('active')
 })
 
-// Drag leave
 dropZone.addEventListener('dragleave', () => {
   dropZone.classList.remove('active')
 })
 
-// Drop file
 dropZone.addEventListener('drop', e => {
   e.preventDefault()
   dropZone.classList.remove('active')
@@ -30,19 +26,17 @@ dropZone.addEventListener('drop', e => {
   }
 })
 
-// File selected via click
 input.addEventListener('change', e => {
   if (e.target.files.length > 0) {
     handleFile(e.target.files[0])
   }
 })
 
-// 🔥 CENTRAL FUNCTION (important fix)
 function handleFile (file) {
   if (!file.name.toLowerCase().endsWith('.csv')) {
     fileInfo.innerHTML = '❌ Invalid file (Only CSV allowed)'
     fileInfo.style.color = 'red'
-    input.value = '' // reset input
+    input.value = '' 
     return
   }
 
@@ -50,7 +44,6 @@ function handleFile (file) {
   fileInfo.style.color = '#22c55e'
 }
 
-// Loader
 form.addEventListener('submit', () => {
   loader.style.display = 'flex'
 })
